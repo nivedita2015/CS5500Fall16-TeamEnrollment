0fd640a7517aaa29c622918fc4ca350e1ea836d1
Nivedita Mittal <Nivi@Niveditas-MacBook-Pro.local>
2016-11-05 13:24:34 -0400

    EN-67 Create test cases for username and password

--- myApp/www/tests/login_test.js
+++ myApp/www/tests/login_test.js
@@ -1,49 +1,76 @@
-//Test for login Ctrl
+// //Test for login Ctrl
+//
+// describe('LoginCtrl', function() {
+//   var scope,state, createController;
+//   beforeEach(module('starter.controllers')); //<--- Hook module
+//
+//   beforeEach(inject(function ($rootScope, $controller,$state) {
+//     scope = $rootScope.$new();
+//     state = $state;
+//     createController = function () {
+//       return $controller('LoginCtrl', {$state:state, $rootScope:$rootScope, $scope:scope});
+//     };
+//   }));
+//
+//   //Test case for correct username-password pair
+//   it('should move to the event page after successfull sign in', function() {
+//     var controller = createController();
+//     spyOn($state,'go');
+//     controller.signin('alice@husky.neu.edu','alice');
+//     expect($state.go).toHaveBeenCalledWith('signin',{emailId:'alice@husky.neu.edu',password:'alice'})
+//   });
+//
+//   // Test case for incorrect username-password pair
+//   it('should throw error because username-password pair is incorrect', function() {
+//     var controller = createController();
+//     controller.signin('alice@husky.neu.edu','bob');
+//     expect(controller.login.error).toEqual('No data found in controller');
+//     spyOn($state,'go');
+//     expect($state.go).toHaveBeenCalledWith('signin',{emailId:'alice@husky.neu.edu',password:'bob'})
+//   });
+//
+//   // Test case for empty username
+//   it('should throw error because username is empty', function() {
+//     var controller = createController();
+//     spyOn($state,'go');
+//     expect($state.go).toHaveBeenCalledWith('signin',{emailId:'',password:'abc'})
+//   });
+//
+//   // Test case for empty password
+//   it('should throw error because password is empty', function() {
+//     var controller = createController();
+//     spyOn($state,'go');
+//     expect($state.go).toHaveBeenCalledWith('signin',{emailId:'charley@neu.edu',password:''})
+//   });
+// });
+
 
 describe('LoginCtrl', function() {
-  var scope,$state,$rootScope, $stateParams, createController;
-  beforeEach(module('starter.controllers')); //<--- Hook module
-  beforeEach(inject(function ($rootScope, $controller,$state) {
-    scope = $rootScope.$new();
+  beforeEach(module('starter.controllers'));
 
-    createController = function() {
-      return $controller('LoginCtrl', {
-        // $state:$state,
-        // $rootScope:$rootScope,
-        $state:$state,
-        $rootScope:$rootScope,
-        $scope: scope
-        // $stateParams:$stateParams
-      });
-    };
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
   }));
 
-  //Test case for correct username-password pair
-  it('should move to the event page after successfull sign in', function() {
-    var controller = createController();
-    spyOn($state,'go');
-    expect($state.go).toHaveBeenCalledWith('signin',{emailId:'alice@husky.neu.edu',password:'alice'})
+
+  //Test case when username-password dont match
+  describe('Login Controller', function() {
+    it('checks the value login.error', function() {
+      var $scope = {};
+      var $state = {};
+      var rootScope = {};
+      var controller = $controller('LoginCtrl', {$state:$state,rootScope:rootScope,$scope: $scope });
+      controller.signin('alice@husky.neu.edu','bob')
+      expect(controller.error).toEqual('No data found in controller');
+    });
   });
 
-  //Test case for incorrect username-password pair
-  it('should throw error because username-password pair is incorrect', function() {
-    var controller = createController();
-    spyOn($state,'go');
-    expect($state.go).toHaveBeenCalledWith('signin',{emailId:'bob',password:'bob'})
-  });
 
-  //Test case for empty username
-  it('should throw error because username is empty', function() {
-    var controller = createController();
-    spyOn($state,'go');
-    expect($state.go).toHaveBeenCalledWith('signin',{emailId:'',password:'abc'})
-  });
-
-  //Test case for empty password
-  it('should throw error because password is empty', function() {
-    var controller = createController();
-    spyOn($state,'go');
-    expect($state.go).toHaveBeenCalledWith('signin',{emailId:'charley@neu.edu',password:''})
-  });
+  //Test case when username
 
 });
+
+
