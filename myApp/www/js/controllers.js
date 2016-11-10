8b91c670e9f14d9701d8016ef086d0861e2ecfa7
Sanamdeep Singh <sanamsoodan@gmail.com>
2016-11-09 23:37:16 -0500

    EN-85 Marker Ctrl

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -135,4 +135,20 @@ angular.module('starter.controllers', ['ui.router'])
   init();
   console.log("inside events details controller");
 
-});
+})
+
+  .controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+    var options = {timeout: 10000, enableHighAccuracy: true};
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+      var latLng = new google.maps.LatLng(42.338452, -71.087834);
+      var mapOptions = {
+        center: latLng,
+        zoom: 17,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    }, function(error){
+      console.log("Could not get location");
+    });
+  })
+;
