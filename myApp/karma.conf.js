c53940ce41b65528192a76da1acefdfba6180414
Sushant Mimani <sm-adaptivealgo@gmail.com>
2016-11-11 16:13:57 -0500

    En-92 Modify coverage to show relevant components

--- myApp/karma.conf.js
+++ myApp/karma.conf.js
@@ -31,7 +31,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.js': ['coverage']
+      'myApp/www/js/*.js': ['coverage'],
+      'myApp/www/tests/*.js': ['coverage']
     },
 
 
