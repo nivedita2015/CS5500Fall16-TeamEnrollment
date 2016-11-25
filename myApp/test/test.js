ffba23f5d1f8de40cb2869495a7ae071627432c9
Sushant Mimani <sm-adaptivealgo@gmail.com>
2016-11-25 10:41:11 -0500

    EN-114, EN-123 Tests for event landing page

--- myApp/test/test.js
+++ myApp/test/test.js
@@ -21,6 +21,7 @@ describe('Login Tests', function() {
     chai.request(server)
       .get('/users?username=alice@husky.neu.edu&password=alice')
       .end(function(err, res){
+	console.log(res.body);
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.have.equal('5835e41872302c8dd48ba5f0');
@@ -32,6 +33,7 @@ describe('Login Tests', function() {
     chai.request(server)
       .get('/users?username=alice@husky.neu.edu&password=test')
       .end(function(err, res){
+	console.log(res.body);
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.have.equal('False');
@@ -43,6 +45,7 @@ describe('Login Tests', function() {
     chai.request(server)
       .get('/users?username=mimani.s@husky.neu.edu&password=test')
       .end(function(err, res){
+	console.log(res.body);
         res.should.have.status(200);
         res.should.be.string;
         res.body.should.have.equal('False');
@@ -52,29 +55,30 @@ describe('Login Tests', function() {
 });
 
 
-// describe('Events Tests', function() {
-//   it('Should return list of events subscribed by a user /users/events GET', function(done) {
-//     chai.request(server)
-//       .get('/users/events?id=5835e41872302c8dd48ba5f0')
-//       .end(function(err, res){
-//         res.should.be.array;
-//         res.should.have.status(200);
+ describe('Events Tests', function() {
+   it('Should return list of events subscribed by a user /users/events GET', function(done) {
+     chai.request(server)
+       .get('/users/events?id=5835e41872302c8dd48ba5f0')
+       .end(function(err, res){
+	 console.log(res.body);
+         res.should.be.array;
+         res.should.have.status(200);
+         assert.lengthOf(res.body,0,'result should have length 0');
+         done();
+       });
+   });
 
-//         done();
-//       });
-//   });
-
-//   it('Should return [] is user has not subscribed to any event /users/events GET', function(done) {
-//     chai.request(server)
-//       .get('/users/events?id=5835e41872302c8dd48ba5f1')
-//       .end(function(err, res){
-//         console.log(res.body);
-//         res.should.be.array;
-//         // res.body
-//         res.should.have.status(200);
+   it('Should return [] is user has not subscribed to any event /users/events GET', function(done) {
+     chai.request(server)
+       .get('/users/events?id=5835e41872302c8dd48ba5f1')
+       .end(function(err, res){
+         console.log(res.body);
+         res.should.be.array;
+         assert.lengthOf(res.body,0,'result should have length 0');
+         res.should.have.status(200);
         
-//         done();
-//       });
-//   });
-// });
+         done();
+       });
+   });
+ });
 
