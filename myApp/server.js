c4a641543ea6bc61638dfde169c72fb6e9a895ec
Sushant Mimani <sm-adaptivealgo@gmail.com>
2016-11-27 16:34:36 -0500

    Fixed indentation

--- myApp/server.js
+++ myApp/server.js
@@ -1,84 +1,126 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var should = chai.should();
-var assert = chai.assert;
-var mongoose = require("mongoose");
-var server =  "http://localhost:8080"
-chai.use(chaiHttp);
+// server.js
 
-// before(function(done) {
-//     // In our tests we use the test db
-//     mongoose.connect('mongodb://ec2-52-207-253-108.compute-1.amazonaws.com:27017/Enrollment')         
-//     done();
-//   });
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var mongoose   = require('mongoose');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(require('body-parser').json());
 
-// after(function(done) {
-//   mongoose.disconnect();
-//   done();
-// });
 
-describe('Login Tests', function() {
-  it('Correct combination should return the userID /users GET', function(done) {
-    chai.request(server)
-      .get('/users?username=alice@husky.neu.edu&password=alice')
-      .end(function(err, res){
-    console.log(res.body);
-        res.should.have.status(200);
-        res.should.be.json;
-        res.body.should.have.equal('5835e41872302c8dd48ba5f0');
-        done();
-      });
-  });
 
-  it('Incorrect combination should return False /users GET', function(done) {
-    chai.request(server)
-      .get('/users?username=alice@husky.neu.edu&password=test')
-      .end(function(err, res){
-    console.log(res.body);
-        res.should.have.status(200);
-        res.should.be.json;
-        res.body.should.have.equal('False');
-        done();
-      });
-  });
+mongoose.connect('mongodb://ec2-52-207-253-108.compute-1.amazonaws.com:27017/Enrollment')
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+   console.log('connected')
+});
 
-  it('A non-existent username should return False /users GET', function(done) {
-    chai.request(server)
-      .get('/users?username=mimani.s@husky.neu.edu&password=test')
-      .end(function(err, res){
-    console.log(res.body);
-        res.should.have.status(200);
-        res.should.be.string;
-        res.body.should.have.equal('False');
-        done();
-      });
-  });
+var userSchema = mongoose.Schema({
+    username : String,
+    password: String
+});
+
+var eventSchema = mongoose.Schema({
+    Name : String,
+    Group : String,
+    Dte : Date,
+    Time : String,
+    Campus : String
+});
+
+var subscribedEventSchema = mongoose.Schema({
+    Events : [String]
+});
+
+var Users = module.exports = mongoose.model('Users', userSchema);
+var Events = module.exports = mongoose.model('Events', eventSchema);
+var Subscribed = module.exports = mongoose.model('SubscribedEvents', subscribedEventSchema);
+
+app.get('/',function(req,res){
+    res.send('Hello World!');
+})
+
+// APIs for user login ----------------------------------------------
+app.get('/users', function(req,res){
+     var uname = req.query.username;
+     var pwd = req.query.password;
+     Users.findOne({'username': uname},function(err,usrs){
+         if (usrs==null) {
+             res.json('False')
+             return;
+         };
+ 
+         if(err){
+             res.json('False');
+             return;
+         }
+         if(usrs.password == pwd){
+ 
+             res.json(usrs.id);
+             return;
+         }
+         res.json('False');
+                 
+     });
+ });
+
+app.post('/users', function(req,res){
+    var user = new Users();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    console.log(user.username);
+    user.save(function(err,user){
+        if(err){
+            throw err;
+        }
+        res.send('User added!')
+    });
+});
+
+// End of APIs for user login ----------------------------------------------
+
+// APIs for Events ---------------------------------------------------------
+
+
+
+app.get('/allEvents', function(req,res){
+    Events.find(function(err,usrs){
+        if(err){
+            throw err;
+        }
+        res.json(usrs)
+    });
 });
 
 
- describe('Events Tests', function() {
-   it('Should return list of events subscribed by a user /users/events GET', function(done) {
-     chai.request(server)
-       .get('/users/events?id=5835e41872302c8dd48ba5f0')
-       .end(function(err, res){
-           console.log(res.body);
-         res.should.be.array;
-         res.should.have.status(200);
-         assert.lengthOf(res.body,2,'result should have length 2');
-         done();
-       });
-   });
+app.get('/users/events', function(req,res){
+    var id = mongoose.Types.ObjectId(req.query.id);
+    var array =  [];
+    Subscribed.findOne({'_id':id},function(err,userEvents){
+        if(err){
+            throw err;
+        }
+        if(userEvents == null){
+          res.send(array);
+          return;
+        }
+    array = userEvents.Events;
+    for (var i = 0; i < array.length; i++) {
+        array[i] = mongoose.Types.ObjectId(array[i]);
+    };
+    Events.find({'_id' : { $in : array}}, function(err, events){
+        res.send(events);
+    });
 
-   it('Should return [] is user has not subscribed to any event /users/events GET', function(done) {
-     chai.request(server)
-       .get('/users/events?id=5835e41872302c8dd48ba5f1')
-       .end(function(err, res){
-        console.log(res.body);
-         res.should.be.array;
-         assert.lengthOf(res.body,0,'result should have length 0');
-         res.should.have.status(200);
-         done();
-       });
-   });
- });
+    });
+});
 
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log('App listening on port 8080');
