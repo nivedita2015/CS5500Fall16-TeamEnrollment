9aff7724b1e7935bb08ad8c01b6b311e8d2cdaec
Sushant Mimani <sm-adaptivealgo@gmail.com>
2016-11-28 01:29:15 -0500

    EN-96 Fix build script

--- myApp/build.sh
+++ myApp/build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+sudo npm install
 sudo npm rebuild node-sass
+sudo kill -9 $(ps aux | grep ionic_server | awk '{print $2}')
 karma start
 python3 json_parser.py
 state="`cat output.txt`"
 if [ "$state" == "failed" ]
 then
         echo "You have failed test cases. Build terminated"
+        forever start www/ionic_server.js
         exit
 else
         echo "Tests successful. Proceeding with build"
-        ionic serve
+        forever start www/ionic_server.js
 fi
